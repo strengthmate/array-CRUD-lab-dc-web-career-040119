cc447073ad785634c6eaaad1bd26b2921b2941ac
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 17:04:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,13 +17,14 @@ def add_element_to_start_of_array(array, element)
     array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(array, element)
     array = ["I", "am", "really", "learning", "arrays!"]
-    array.pop()
+    array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
   
+  array.unshift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
