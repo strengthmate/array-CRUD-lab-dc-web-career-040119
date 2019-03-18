bf5c14220381e315bb797d42eb3c419bfb93f9a7
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 17:08:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  
 end
 
 def add_element_to_end_of_array(array, element)
@@ -28,7 +28,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-    array[index_number]
+    element = array[index_number]
+    element
 end
 
 def retrieve_first_element_from_array(array)
